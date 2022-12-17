3375fd475d0247a2f32d4a8face301d088330911
comerc <andrew.kachanov@gmail.com>
2022-12-17 16:32:37 +0300

    Add /api/countdown

---
+++ pages/api/countdown.ts
@@ -0,0 +1,42 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const DateDiff = {
+  inDays: function(d1, d2) {
+      const t2 = d2.getTime()
+      const t1 = d1.getTime()
+      return Math.floor((t2-t1)/(24*3600*1000))
+  },
+  inWeeks: function(d1, d2) {
+      const t2 = d2.getTime()
+      const t1 = d1.getTime()
+      return parseInt((t2-t1)/(24*3600*1000*7))
+  },
+  inMonths: function(d1, d2) {
+      const d1Y = d1.getFullYear()
+      const d2Y = d2.getFullYear()
+      const d1M = d1.getMonth()
+      const d2M = d2.getMonth()
+      return (d2M+12*d2Y)-(d1M+12*d1Y)
+  },
+  inYears: function(d1, d2) {
+      return d2.getFullYear()-d1.getFullYear()
+  }
+}
+
+type Data = {
+  data: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const dString = "Apr, 2, 2023"
+  const d1 = new Date(dString)
+  const d2 = new Date()
+  const diff = DateDiff.inDays(d1, d2)
+  res.status(200).json({ data: `Number of days since ${dString} ${diff}` })
+}
+
+
+
