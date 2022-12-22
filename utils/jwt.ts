ceeae4148796e6551a3d4a0c87e71cd08f99279b
comerc <andrew.kachanov@gmail.com>
2022-12-22 19:59:39 +0300

    Remove unused code

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -8,9 +8,6 @@ export const encode = async ({ secret, token }) => {
   if (!token) {
     return ''
   }
-  // if (token!.state) {
-  //   return jsonwebtoken.sign(token, secret)
-  // }
   const encodedToken = await new jose.SignJWT(token)
     .setProtectedHeader({ alg: 'HS256' })
     .sign(convertToUint8Array(secret))
@@ -22,11 +19,11 @@ export const decode = async ({ secret, token }) => {
   if (!token) {
     return null
   }
-  try {
-    const { payload } = await jose.jwtVerify(token, convertToUint8Array(secret))
-    return payload as JWT
-  } catch (error) {
-    console.log(error)
-    // JWT validation failed or token is invalid
-  }
+  // try {
+  const { payload } = await jose.jwtVerify(token, convertToUint8Array(secret))
+  return payload as JWT
+  // } catch (error) {
+  //   console.log(error)
+  //   // JWT validation failed or token is invalid
+  // }
 }
