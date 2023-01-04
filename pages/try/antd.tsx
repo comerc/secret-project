d04b56becc3501911f7a6b1dbf1f6995338146da
comerc <andrew.kachanov@gmail.com>
2023-01-05 02:00:12 +0300

    Fix tailwind & antd

--- pages/try/antd.tsx
+++ pages/try/antd.tsx
@@ -1,7 +1,8 @@
-import { Button, DatePicker, Form, InputNumber, Select, Slider, Switch } from 'antd'
+import { Button, DatePicker, Form, InputNumber, Select, Slider, Switch, Modal } from 'antd'
 import type { DatePickerProps } from 'antd'
 import { SmileFilled } from '@ant-design/icons'
 import Link from 'next/link'
+import React from 'react'
 
 const FormItem = Form.Item
 
@@ -12,74 +13,105 @@ function TryAntdPage() {
   const onDatePickerChange: DatePickerProps['onChange'] = (date, dateString) => {
     console.log(date, dateString)
   }
+  const [open, setOpen] = React.useState(false)
 
   return (
-    <div style={content}>
-      <div className="text-center mb-5">
-        <Link href="#" className="logo mr-0">
-          <SmileFilled style={{ fontSize: 48 }} />
-        </Link>
+    <>
+      <div style={content}>
+        <div className="mb-5 text-center">
+          <Link href="#" className="logo mr-0">
+            <div className="h-20">
+              <SmileFilled style={{ fontSize: 48 }} />
+            </div>
+          </Link>
+          <p className="text-disabled mb-3 mt-3">Welcome to the world !</p>
+          <Button type="primary" onClick={() => setOpen(true)}>
+            Open Modal of 1000px width
+          </Button>
+        </div>
+        <div>
+          <Form layout="horizontal" size={'large'} labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
+            <FormItem label="Input Number">
+              <InputNumber
+                min={1}
+                max={10}
+                style={{ width: 100 }}
+                defaultValue={3}
+                name="inputNumber"
+              />
+            </FormItem>
 
-        <p className="mb-0 mt-3 text-disabled">Welcome to the world !</p>
+            <FormItem label="Switch">
+              <Switch defaultChecked />
+            </FormItem>
+
+            <FormItem label="Slider">
+              <Slider defaultValue={70} />
+            </FormItem>
+
+            <FormItem label="Select">
+              <Select
+                defaultValue="lucy"
+                style={{ width: 192 }}
+                options={[
+                  {
+                    value: 'jack',
+                    label: 'Jack',
+                  },
+                  {
+                    value: 'lucy',
+                    label: 'Lucy',
+                  },
+                  {
+                    value: 'disabled',
+                    disabled: true,
+                    label: 'Disabled',
+                  },
+                  {
+                    value: 'Yiminghe',
+                    label: 'yiminghe',
+                  },
+                ]}
+              />
+            </FormItem>
+
+            <FormItem label="DatePicker">
+              <DatePicker showTime onChange={onDatePickerChange} />
+            </FormItem>
+            <FormItem style={{ marginTop: 48 }} wrapperCol={{ offset: 8 }}>
+              <Button type="primary" htmlType="submit">
+                OK
+              </Button>
+              <Button
+                style={{ marginLeft: 8 }}
+                onClick={() =>
+                  Modal.confirm({
+                    content: 'Are you sure you want to clear the editor?',
+                    maskClosable: true,
+                    afterClose() {},
+                    onOk() {},
+                  })
+                }
+              >
+                Cancel
+              </Button>
+            </FormItem>
+          </Form>
+        </div>
       </div>
-      <div>
-        <Form layout="horizontal" size={'large'} labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
-          <FormItem label="Input Number">
-            <InputNumber
-              min={1}
-              max={10}
-              style={{ width: 100 }}
-              defaultValue={3}
-              name="inputNumber"
-            />
-          </FormItem>
-
-          <FormItem label="Switch">
-            <Switch defaultChecked />
-          </FormItem>
-
-          <FormItem label="Slider">
-            <Slider defaultValue={70} />
-          </FormItem>
-
-          <FormItem label="Select">
-            <Select
-              defaultValue="lucy"
-              style={{ width: 192 }}
-              options={[
-                {
-                  value: 'jack',
-                  label: 'Jack',
-                },
-                {
-                  value: 'lucy',
-                  label: 'Lucy',
-                },
-                {
-                  value: 'disabled',
-                  disabled: true,
-                  label: 'Disabled',
-                },
-                {
-                  value: 'Yiminghe',
-                  label: 'yiminghe',
-                },
-              ]}
-            />
-          </FormItem>
-
-          <FormItem label="DatePicker">
-            <DatePicker showTime onChange={onDatePickerChange} />
-          </FormItem>
-          <FormItem style={{ marginTop: 48 }} wrapperCol={{ offset: 8 }}>
-            <Button type="primary" htmlType="submit">
-              OK
-            </Button>
-            <Button style={{ marginLeft: 8 }}>Cancel</Button>
-          </FormItem>
-        </Form>
-      </div>
-    </div>
+      <Modal
+        title="Modal 1000px width"
+        centered
+        open={open}
+        onOk={() => setOpen(false)}
+        onCancel={() => setOpen(false)}
+        width={1000}
+      >
+        <p>some contents...</p>
+        <p>some contents...</p>
+        <p>some contents...</p>
+      </Modal>
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
 module.exports = {
+  relative: true,
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -8,9 +10,37 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography')],
-  corePlugins: {
-    preflight: false, // https://stackoverflow.com/questions/71715157/tailwinds-ant-design-button-color-is-white-but-has-own-color-wnen-i-hover-it
-  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+    plugin(function ({ addBase, addComponents, addUtilities, theme }) {
+      // addBase({
+      //   h1: {
+      //     fontSize: theme('fontSize.2xl'),
+      //   },
+      //   h2: {
+      //     fontSize: theme('fontSize.xl'),
+      //   },
+      // })
+      // addComponents({
+      //   '.card': {
+      //     backgroundColor: theme('colors.white'),
+      //     borderRadius: theme('borderRadius.lg'),
+      //     padding: theme('spacing.6'),
+      //     boxShadow: theme('boxShadow.xl'),
+      //   },
+      // })
+      // addUtilities({
+      //   '.content-auto': {
+      //     contentVisibility: 'auto',
+      //   },
+      // })
+    }),
+  ],
+  // corePlugins: {
+  //   preflight: false, // https://stackoverflow.com/questions/71715157/tailwinds-ant-design-button-color-is-white-but-has-own-color-wnen-i-hover-it
+  // }, // костыль нужен только для Modal.confirm(), для обычного Modal работает без него
   // darkMode: false,
+  important: true,
 }
