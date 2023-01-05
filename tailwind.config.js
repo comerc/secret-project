e7ecf84e57d9f2efb48d5fa1f625291c46ed8ad6
comerc <andrew.kachanov@gmail.com>
2023-01-05 22:52:34 +0300

    Add @tailwindcss/container-queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/container-queries'),
     plugin(function ({ addBase, addComponents, addUtilities, theme }) {
       // addBase({
       //   h1: {
