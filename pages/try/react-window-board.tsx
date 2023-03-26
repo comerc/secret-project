893c84ccb938411cf1598f92ff9b8449c6705de1
comerc <andrew.kachanov@gmail.com>
2023-03-26 19:28:07 +0300

    Fix setHasMenu()

--- pages/try/react-window-board.tsx
+++ pages/try/react-window-board.tsx
@@ -129,7 +129,7 @@ function TryReactWindowBoardPage() {
               onClick={() => {
                 setIsMenu(!isMenu)
                 setTimeout(() => {
-                  setIsMenuDummy(!hasMenu)
+                  setHasMenu(!hasMenu)
                 })
               }}
             >
