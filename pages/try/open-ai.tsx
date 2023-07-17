fad81e257c69523d70e599450dabd21e138d7a9a
comerc <andrew.kachanov@gmail.com>
2023-07-17 18:08:21 +0300

    Add /try/open-ai

---
+++ pages/try/open-ai.tsx
@@ -0,0 +1,19 @@
+// const { Configuration, OpenAIApi } = require("openai");
+
+// const configuration = new Configuration({
+//     apiKey: 'YOUR_API_KEY',
+//   });
+
+//   const openai = new OpenAIApi(configuration);
+//   const response = await openai.createCompletion({
+//     model: "text-davinci-003",
+//     prompt: ask,
+//     temperature: 1.0,
+//     max_tokens: 3917,
+//     top_p: 1,
+//     frequency_penalty: 0,
+//     presence_penalty: 0,
+//   });
+
+//   var responsebody = response['data'].choices[0]
+//   return responsebody
